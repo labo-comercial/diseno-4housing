51e32526a5fbe3144809a3cb966f2c889863c98e
labo-comercial <pablospinetto@4housing.com.ar>
2026-06-24 08:59:23 -0300

    Delete public directory

--- public/js/config.js
+++
@@ -1,2 +0,0 @@
-window.SUPABASE_URL = "https://nfgjxvatfihjrtktivhq.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZ2p4dmF0ZmloanJ0a3RpdmhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMzI1MzcsImV4cCI6MjA5NzgwODUzN30.1A4M7QjZwl5Yd6lq1gn0itvhyuTxryQRakWBZJ8lA-Y";
